c35e5fa0f9698050bdd3426ebc2023a1b175774c
BishwasKoirala <wasubisu69@gmail.com>
2024-03-06 17:18:17 +0900

    get optimized images

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -4,6 +4,7 @@ import { Game } from '../hooks/useGames'
 import { Card, CardBody, HStack, Heading, Image, Text } from '@chakra-ui/react'
 import PlatformiconList from './PlatformiconList'
 import CriticScore from './CriticScore'
+import getCroppedImageUrl from '../services/image-url'
 
 
 interface Props{
@@ -13,7 +14,8 @@ interface Props{
 const GameCard = ({game} : Props) => {
   return (
     <Card borderRadius={10} overflow="hidden">
-      <Image src={game.background_image}></Image>
+      {/* passing the cropped image with the image url to crop */}
+      <Image src={getCroppedImageUrl(game.background_image)}></Image>
       <CardBody>
         <Heading fontSize="2xl">{game.name}</Heading>
         {/* destructuring and getting the prop of platform */}
---
+++ src/services/image-url.ts
@@ -0,0 +1,11 @@
+const getCroppedImageUrl = (url : string) => {
+  const target = 'media/'
+  const index = url.indexOf(target) + target.length 
+  return url.slice(0,index) + 'crop/600/400/' + url.slice(index)
+  // url.slice(9,index) will slice it from http~~to~~media/ 
+  //then the 'crop/600/400' is added
+  // then adding url.slice(index) will slice the url from games/20~~to~~64.jpg and add it up
+
+}
+
+export default getCroppedImageUrl;
