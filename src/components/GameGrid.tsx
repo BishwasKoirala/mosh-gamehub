81addf0ebd4c8d77ef6854441303a6b0a56eed22
BishwasKoirala <wasubisu69@gmail.com>
2024-03-09 21:07:35 +0900

    displaying the game genre

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -13,18 +13,18 @@ const GameGrid = () => {
       {/* passing sm , md ,lg means how the size should be depending on screen size */}
       <SimpleGrid
         columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
-        spacing={10}
+        spacing={3}
         padding="10px"
       >
         {isLoading &&
           skeletons.map((skeletons) => (
-            <GameCardContainer>
-              <GameCardSkeleton key={skeletons} />
+            <GameCardContainer key={skeletons} >
+              <GameCardSkeleton />
             </GameCardContainer>
           ))}
         {data.map((game) => (
-          <GameCardContainer>
-            <GameCard key={game.id} game={game} />
+          <GameCardContainer key={game.id}>
+            <GameCard  game={game} />
           </GameCardContainer>
         ))}
       </SimpleGrid>
--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,15 +1,31 @@
-import useGenres from '../hooks/useGenres'
+import { Button, HStack, Image, List, ListItem, Spinner, Text } from "@chakra-ui/react";
+import useGenres from "../hooks/useGenres";
+import getCroppedImageUrl from "../services/image-url";
 
 const GenreList = () => {
+  const { data , isLoading , error } = useGenres();
 
-  const {data} = useGenres()
+  if (error) return null
+  if (isLoading) 
+    return <Spinner />
+  
 
   return (
-    <ul>
-    {data.map(genre => <li key={genre.id}>{genre.name}</li> )}
-    </ul>
+    <List>
+      {data.map((genre) => (
+        <ListItem key={genre.id} paddingY='5px'>
+          <HStack>
+            <Image
+              boxSize="32px"
+              borderRadius={8}
+              src={getCroppedImageUrl(genre.image_background)}
+            />
+            <Button onClick={() => console.log(genre)} fontSize='large' variant='link'>{genre.name}</Button>
+          </HStack>
+        </ListItem>
+      ))}
+    </List>
+  );
+};
 
-  )
-}
-
-export default GenreList
+export default GenreList;
